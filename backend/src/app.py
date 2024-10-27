06ff97f26b51f97b4baf08a275011e683e2421e6
KESHOKUMAR D <keshokumar4147@gmail.com>
2024-10-27 15:06:36 +0530

    port updation

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
@@ -27,4 +28,5 @@ def predict():
     return jsonify({'prediction': label, 'probability': float(prediction[0][0])})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Make sure Flask listens on all interfaces
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT env variable if provided, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
